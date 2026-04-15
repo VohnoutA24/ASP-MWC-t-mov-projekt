7d6d51b457ba90d08321ae6e9ee5680a2d6cecf9
eluvvs <vohnouta24@spstrutnov.cz>
2026-04-15 10:04:25 +0200

    solution creation

---
+++ sum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
